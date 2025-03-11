31bc82fbf8e132207d19468404768044200218ef
dml <dmlisgud@gmail.com>
2025-03-11 19:38:46 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Alamutu Mubarak | Full-Stack Developer</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../bootstrap/dist/css/bootstrap.min.css">
     <style>
         :root {
